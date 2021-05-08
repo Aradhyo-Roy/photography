047be339655e34d7024f9db3efab3ae05b6ed871
Aradhyo-Roy <83859573+Aradhyo-Roy@users.noreply.github.com>
2021-05-08 14:17:54 +0530

    Add files via upload

---
+++ photographer.js
@@ -0,0 +1,12 @@
+function alertbox(){
+    window.alert("Saplings is an adult novel by Noel Streatfeild, published by Collins, about the Wiltshire middle-class family living in the Regent's Park in pre-Second World War London. With the breakdown of society under German attack, the family undergoes its own rapid disintegration. The novel is written from the perspective of four children - Laurel, Tony, Tuesday, and Kim, as well as from the perspective of their mother, Lena.")
+}
+function alertbox2(){
+    window.alert("A shade tree is a large tree whose primary role is to provide shade in the surrounding environment due to its spreading canopy and crown, where it may give shelter from sunlight in the heat of the summer for people who seek recreational needs in urban parks and house yards, and thus, also protecting them from the sun's harmful UV rays and sunburns. Therefore, some shade trees may be grown specifically for the comfort of the population due to their convenient shelter.")
+}
+function alertbox3(){
+    window.alert("A mountain is an elevated portion of the Earth's crust, generally with steep sides that show significant exposed bedrock. A mountain differs from a plateau in having a limited summit area, and is larger than a hill, typically rising at least 300 metres (1000 feet) above the surrounding land. A few mountains are isolated summits, but most occur in mountain ranges")
+}
+function alertbox4(){
+    window.alert("A forest is an area of land dominated by trees.Hundreds of definitions of forest are used throughout the world, incorporating factors such as tree density, tree height, land use, legal standing and ecological function.The Food and Agriculture Organization defines a forest as land spanning more than 0.5 hectares with trees higher than 5 meters and a canopy cover of more than 10 percent, or trees able to reach these thresholds in situ. It does not include land that is predominantly under agricultural or urban land use.Using this definition FRA 2020 found that forests covered 4.06 billion hectares or approximately 31 percent of the global land area in 2020 but are equally distributed around the globe.")
+}
